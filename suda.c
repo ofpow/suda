cef77091e1eeda60d7ca7e0f29458f9412a1d1e9
Oliver Powell <oliver@drpowell.org>
2023-10-01 13:03:36 -0400

    Change Parser to pointer to be use *_TOK macros

--- suda.c
+++ suda.c
@@ -54,7 +54,7 @@ float strtofloat(const char *str, int len) {
 //global variables for access to freeing from anywhere
 Token *tokens;
 char *program;
-Parser p;
+Parser *p;
 //Node **nodes;
 //int nodes_index;
 Interpreter interpreter;
@@ -64,7 +64,7 @@ void free_mem(int exit_val) {
     debug("\n----------\nFREEING\n")
 
     free(tokens);
-    free(p.jump_indices);
+    free(p->jump_indices);
     for (int i = 0; i < interpreter.vars_index; i++) {
         if (interpreter.vars[i].value->type == Value_Array) {
             int arr_len = (int)strtofloat(interpreter.vars[i].value[0].value, strlen(interpreter.vars[i].value[0].value));
@@ -84,8 +84,9 @@ void free_mem(int exit_val) {
     free(interpreter.vars);
     for (int i = 0; i < interpreter.funcs_capacity; i++) free_function(interpreter.funcs[i]);
     free(interpreter.funcs);
-    for (int i = 0; i < p.nodes_index; i++) free_node(p.nodes[i]);
-    free(p.nodes);
+    for (int i = 0; i < p->nodes_index; i++) free_node(p->nodes[i]);
+    free(p->nodes);
+    free(p);
     free(program);
     exit(exit_val);
 }
@@ -140,20 +141,21 @@ int main(int argc, char *argv[]) {
     debug("\n----------\nPARSING\n")
 
     //parse tokens
-    p.tokens = tokens;
-    p.tok_index = 0;
-    p.jumps_index = 0;
-    p.jumps_capacity = 10;
-    p.jump_indices = calloc(10, sizeof(int));
+    p = calloc(1, sizeof(Parser));
+    p->tokens = tokens;
+    p->tok_index = 0;
+    p->jumps_index = 0;
+    p->jumps_capacity = 10;
+    p->jump_indices = calloc(10, sizeof(int));
 
-    p.nodes_index = 0;
-    p.nodes_capacity = 10;
+    p->nodes_index = 0;
+    p->nodes_capacity = 10;
 
-    p.nodes = calloc(p.nodes_capacity, sizeof(Node*));
+    p->nodes = calloc(p->nodes_capacity, sizeof(Node*));
     
-    p.funcs_index = 0;
-    p.funcs_capacity = 10;
-    p.funcs = calloc(p.funcs_capacity, sizeof(Function*));
+    p->funcs_index = 0;
+    p->funcs_capacity = 10;
+    p->funcs = calloc(p->funcs_capacity, sizeof(Function*));
 
     //variables to store main state when parsing a function
     Function *func;
@@ -162,73 +164,73 @@ int main(int argc, char *argv[]) {
     Node **temp_nodes = NULL;
 
     while (1) {
-        if (p.nodes_index >= p.nodes_capacity) {
-            p.nodes_capacity *= 2;
-            p.nodes = realloc(p.nodes, p.nodes_capacity * sizeof(Node*));
+        if (p->nodes_index >= p->nodes_capacity) {
+            p->nodes_capacity *= 2;
+            p->nodes = realloc(p->nodes, p->nodes_capacity * sizeof(Node*));
         }
-        Node *n = statement(&p);
+        Node *n = statement(p);
         if (n->type == AST_End) {
             free_node(n);
-            if (p.jumps_index > 0) {
+            if (p->jumps_index > 0) {
                 ERR("unclosed block\n")
             }
             break;
         } else if (n->type == AST_If) {
-            append(p.jump_indices, p.nodes_index, p.jumps_index, p.jumps_capacity)
-            append(p.nodes, n, p.nodes_index, p.nodes_capacity)
+            append(p->jump_indices, p->nodes_index, p->jumps_index, p->jumps_capacity)
+            append(p->nodes, n, p->nodes_index, p->nodes_capacity)
         } else if (n->type == AST_While) {
-            append(p.jump_indices, p.nodes_index, p.jumps_index, p.jumps_capacity)
-            append(p.nodes, n, p.nodes_index, p.nodes_capacity)
+            append(p->jump_indices, p->nodes_index, p->jumps_index, p->jumps_capacity)
+            append(p->nodes, n, p->nodes_index, p->nodes_capacity)
         } else if (n->type == AST_Else) {
-            p.nodes[p.jump_indices[p.jumps_index - 1]]->jump_index = p.nodes_index;
-            n->jump_index = p.jump_indices[p.jumps_index - 1];
-            p.jumps_index--;
-            append(p.jump_indices, p.nodes_index, p.jumps_index, p.jumps_capacity)
-            append(p.nodes, n, p.nodes_index, p.nodes_capacity)
+            p->nodes[p->jump_indices[p->jumps_index - 1]]->jump_index = p->nodes_index;
+            n->jump_index = p->jump_indices[p->jumps_index - 1];
+            p->jumps_index--;
+            append(p->jump_indices, p->nodes_index, p->jumps_index, p->jumps_capacity)
+            append(p->nodes, n, p->nodes_index, p->nodes_capacity)
         } else if (n->type == AST_Semicolon) {
-            if (p.parsing_function && p.jumps_index <= 0) {
+            if (p->parsing_function && p->jumps_index <= 0) {
                 free_node(n);
-                func->nodes = p.nodes;
-                func->nodes_size = p.nodes_index;
-                append(p.funcs, func, p.funcs_index, p.funcs_capacity);
+                func->nodes = p->nodes;
+                func->nodes_size = p->nodes_index;
+                append(p->funcs, func, p->funcs_index, p->funcs_capacity);
 
-                p.nodes = temp_nodes;
-                p.nodes_index = temp_nodes_index;
-                p.nodes_capacity = temp_nodes_capacity;
+                p->nodes = temp_nodes;
+                p->nodes_index = temp_nodes_index;
+                p->nodes_capacity = temp_nodes_capacity;
                 temp_nodes = NULL;
                 continue;
             }
 
-            p.jumps_index--;
-            p.nodes[p.jump_indices[p.jumps_index]]->jump_index = p.nodes_index;
-            n->jump_index = p.jump_indices[p.jumps_index];
-            append(p.nodes, n, p.nodes_index, p.nodes_capacity)
+            p->jumps_index--;
+            p->nodes[p->jump_indices[p->jumps_index]]->jump_index = p->nodes_index;
+            n->jump_index = p->jump_indices[p->jumps_index];
+            append(p->nodes, n, p->nodes_index, p->nodes_capacity)
         } else if (n->type == AST_Function) {
             free_node(n);
             func = calloc(1, sizeof(Function));
-            func->name = format_str(p.tokens[p.tok_index].length + 1, "%.*s", p.tokens[p.tok_index].length, p.tokens[p.tok_index].start);
+            func->name = format_str(CURRENT_TOK.length + 1, "%.*s", CURRENT_TOK.length, CURRENT_TOK.start);
 
-            p.tok_index++;
-            ASSERT((p.jumps_index >= 0), "ERROR: unclosed block before function %s\n", func->name)
-            ASSERT((p.tokens[p.tok_index].type == Tok_Left_Paren), "ERROR on line %d: need left paren to open function arguments\n", p.tokens[p.tok_index].line)
-            p.tok_index++;
+            p->tok_index++;
+            ASSERT((p->jumps_index >= 0), "ERROR: unclosed block before function %s\n", func->name)
+            ASSERT((CURRENT_TOK.type == Tok_Left_Paren), "ERROR on line %d: need left paren to open function arguments\n", CURRENT_TOK.line)
+            p->tok_index++;
 
             //switch parser to parse the function
             if (temp_nodes != NULL) ERR("ERROR cant define functions inside other functions\n")
-            temp_nodes = p.nodes;
-            temp_nodes_index = p.nodes_index;
-            temp_nodes_capacity = p.nodes_capacity;
-            p.nodes = calloc(10, sizeof(Node*));
-            p.nodes_index = 0;
-            p.nodes_capacity = 10;
-            p.parsing_function = 1;
+            temp_nodes = p->nodes;
+            temp_nodes_index = p->nodes_index;
+            temp_nodes_capacity = p->nodes_capacity;
+            p->nodes = calloc(10, sizeof(Node*));
+            p->nodes_index = 0;
+            p->nodes_capacity = 10;
+            p->parsing_function = 1;
 
             //parse arguments into func->args
             int args_capacity = 1;
             func->arity = 0;
             func->args = calloc(args_capacity, sizeof(AST_Value*));
             while (1) {
-                Node *n = expr(&p, NULL);
+                Node *n = expr(p, NULL);
                 if (n->type == AST_Identifier) {
                     append(func->args, n->value, func->arity, args_capacity)
                     n->value = NULL;
@@ -239,21 +241,21 @@ int main(int argc, char *argv[]) {
                 } else if (n->type == AST_Right_Paren) {
                     free_node(n);
                     break;
-                } else ERR("ERROR on line %d: cant parse token type %s as part of function arguments\n", p.tokens[p.tok_index].line, find_tok_type(p.tokens[p.tok_index].type))
+                } else ERR("ERROR on line %d: cant parse token type %s as part of function arguments\n", CURRENT_TOK.line, find_tok_type(CURRENT_TOK.type))
             }
         } else {
-            append(p.nodes, n, p.nodes_index, p.nodes_capacity)
+            append(p->nodes, n, p->nodes_index, p->nodes_capacity)
         }
     }
 
     debug("\n----------\nINTERPRETING\n")
 
-    interpreter.nodes = p.nodes;
-    interpreter.stmts_capacity = p.nodes_index;
+    interpreter.nodes = p->nodes;
+    interpreter.stmts_capacity = p->nodes_index;
     interpreter.vars_index = 0;
     interpreter.vars = calloc(10, sizeof(struct Variable));
-    interpreter.funcs_capacity = p.funcs_index;
-    interpreter.funcs = p.funcs;
+    interpreter.funcs_capacity = p->funcs_index;
+    interpreter.funcs = p->funcs;
     interpret(&interpreter);
 
     free_mem(0);
