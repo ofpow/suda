d4e0a5f14eff5414e007b9068b1ad276245af06f
Oliver Powell <oliver@drpowell.org>
2023-10-08 23:03:22 -0400

    Add checking for redefinition of functions

--- function.h
+++ function.h
@@ -7,3 +7,10 @@ Function *get_func(Function **funcs, int funcs_size, char *func_name) {
     ERR("ERROR: function %s not found\n", func_name);
     return NULL;
 }
+
+int check_func(Function **funcs, int funcs_size, char *func_name) {
+    for (int i = 0; i < funcs_size; i++) {
+        if (!strcmp(funcs[i]->name, func_name)) return i;
+    }
+    return -1;
+}
