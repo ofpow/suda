b12a951530659759dcc27817438866196d6d9a89
Oliver Powell <oliver@drpowell.org>
2023-11-17 12:55:06 -0500

    Fix not equal comparison

--- interpreter.h
+++ interpreter.h
@@ -183,7 +183,7 @@ AST_Value *ast_math(AST_Value *op1, AST_Value *op2, int op, int line, const char
         case AST_Not:
             return new_ast_value(Value_Number, format_str(2, "%d", !op1->value), 1);
         case AST_Not_Equal:
-            return new_ast_value(Value_Number, format_str(2, "%d", strcmp(op1->value, op2->value)), 1);
+            return new_ast_value(Value_Number, format_str(2, "%d", !!strcmp(op1->value, op2->value)), 1);
         case AST_Modulo:
             ASSERT((op1->type == Value_Number && op2->type == Value_Number), "ERROR in %s on line %d: Cant modulo type %s and type %s\n", file, line, find_ast_value_type(op1->type), find_ast_value_type(op2->type))
             return new_ast_value(Value_Number, format_str(op1_len + op2_len + 1, "%d", strtoint(op1->value, op1_len) % (int)strtoint(op2->value, op2_len)), 1);
