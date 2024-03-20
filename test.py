776d4ba7a23f1edc40621d13053629509127fa02
Oliver Powell <oliver@drpowell.org>
2024-03-20 10:15:41 -0400

    Update tests to work with bytecode vm

--- test.py
+++ test.py
@@ -3,32 +3,49 @@ import sys
 import os
 import subprocess
 
-sucesses, failures = 0, 0
+def record():
+    for entry in os.scandir("./tests"):
+        if entry.is_file() and entry.path.endswith(".suda"):
+            print(f"[CMD] suda {entry.path} > {entry.path[:-len('.suda')] + '.txt'}")
+            output = subprocess.run(["./suda", entry.path], capture_output=True, text=True)
+            if output.returncode == 0:
+                with open((entry.path[:-len(".suda")] + ".txt"), "w") as f:
+                    f.write(output.stdout)
+            else:
+                print(f"[ERR] {entry.path} exited with non-zero exit code")
+
+def run_tests(bytecode):
+    sucesses, failures = 0, 0
+    if bytecode:
+        cmd = ["./suda", "-b"]
+    else:
+        cmd = ["./suda"]
+
+    for entry in os.scandir("./tests"):
+        if entry.is_file() and entry.path.endswith(".suda"):
+            print(f"[TEST] {entry.path}")
+            if os.path.isfile(entry.path[:-len(".suda")] + ".txt"):
+                with open (entry.path[:-len(".suda")] + ".txt", "r") as f:
+                    cmd.append(entry.path)
+                    if (subprocess.run(cmd, capture_output=True, text=True).stdout == f.read()):
+                        sucesses += 1
+                    else:
+                        print(f"[FAIL] test {entry.path} failed")
+                        failures += 1
+                    cmd.pop()
+            else:
+                print(f"[ERR] test {entry.path} has no expected output")
+    if not bytecode:
+        print(f"TREE WALK: {sucesses} tests succeded and {failures} tests failed")
+    else:
+        print(f"BYTECODE:  {sucesses} tests succeded and {failures} tests failed")
 
 if __name__ == "__main__":
     print("[CMD] gcc -o suda suda.c")
     subprocess.run(["gcc", "-o", "suda", "suda.c"])
     if len(sys.argv) != 2:
-        for entry in os.scandir("./tests"):
-            if entry.is_file() and entry.path.endswith(".suda"):
-                print(f"[TEST] {entry.path}")
-                if os.path.isfile(entry.path[:-len(".suda")] + ".txt"):
-                    with open (entry.path[:-len(".suda")] + ".txt", "r") as f:
-                        if (subprocess.run(["./suda", entry.path], capture_output=True, text=True).stdout == f.read()):
-                            sucesses += 1
-                        else:
-                            print(f"[FAIL] test {entry.path} failed")
-                            failures += 1
-                else:
-                    print(f"[ERR] test {entry.path} has no expected output")
-        print(f"{sucesses} tests succeded and {failures} tests failed")
+        run_tests(False)
+    elif sys.argv[1].startswith('b'):
+        run_tests(True)
     elif sys.argv[1] == 'record':
-        for entry in os.scandir("./tests"):
-            if entry.is_file() and entry.path.endswith(".suda"):
-                print(f"[CMD] suda {entry.path} > {entry.path[:-len('.suda')] + '.txt'}")
-                output = subprocess.run(["./suda", entry.path], capture_output=True, text=True)
-                if output.returncode == 0:
-                    with open((entry.path[:-len(".suda")] + ".txt"), "w") as f:
-                        f.write(output.stdout)
-                else:
-                    print(f"[ERR] {entry.path} exited with non-zero exit code")
+        record_tests()
