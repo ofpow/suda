8da25fef36a2a2bee4c95863c1dbaccd2dd2aaac
Oliver Powell <oliver@drpowell.org>
2025-06-27 01:05:24 -0400

    ord and chr native functions

--- src/native.h
+++ src/native.h
@@ -1,3 +1,7 @@
+#pragma once
+
+char chars[] = {' ', 0, '!', 0, '"', 0, '#', 0, '$', 0, '%', 0, '&', 0, '\'', 0, '(', 0, ')', 0, '*', 0, '+', 0, ',', 0, '-', 0, '.', 0, '/', 0, '0', 0, '1', 0, '2', 0, '3', 0, '4', 0, '5', 0, '6', 0, '7', 0, '8', 0, '9', 0, ':', 0, ';', 0, '<', 0, '=', 0, '>', 0, '?', 0, '@', 0, 'A', 0, 'B', 0, 'C', 0, 'D', 0, 'E', 0, 'F', 0, 'G', 0, 'H', 0, 'I', 0, 'J', 0, 'K', 0, 'L', 0, 'M', 0, 'N', 0, 'O', 0, 'P', 0, 'Q', 0, 'R', 0, 'S', 0, 'T', 0, 'U', 0, 'V', 0, 'W', 0, 'X', 0, 'Y', 0, 'Z', 0, '[', 0, '\\', 0, ']', 0, '^', 0, '_', 0, '`', 0, 'a', 0, 'b', 0, 'c', 0, 'd', 0, 'e', 0, 'f', 0, 'g', 0, 'h', 0, 'i', 0, 'j', 0, 'k', 0, 'l', 0, 'm', 0, 'n', 0, 'o', 0, 'p', 0, 'q', 0, 'r', 0, 's', 0, 't', 0, 'u', 0, 'v', 0, 'w', 0, 'x', 0, 'y', 0, 'z', 0, '{', 0, '|', 0, '}', 0, '~'};
+
 typedef struct Location {
     const char *file;
     int64_t line;
@@ -103,6 +107,32 @@ Value pop(Value *args, Location loc) {
     return (Value) {0};
 }
 
+Value ord(Value *args, Location loc) {
+    if (args[0].type == Value_String) {
+        return (Value){
+            Value_Number,
+            .val.num=args[0].val.str.chars[0],
+            false,
+            0
+        };
+    } else ERR("ERROR in %s on line %ld: cant do ord of type %s\n", loc.file, loc.line, find_value_type(args[0].type))
+
+    return (Value) {0};
+}
+
+Value chr(Value *args, Location loc) {
+    if (args[0].type == Value_Number) {
+        return (Value){
+            Value_String,
+            .val.str={&chars[(args[0].val.num - 32) * 2], 1},
+            false,
+            0
+        };
+    } else ERR("ERROR in %s on line %ld: cant do chr of type %s\n", loc.file, loc.line, find_value_type(args[0].type))
+
+    return (Value) {0};
+}
+
 typedef Value (*Native)(Value*, Location);
 
 //    c function name     suda function name     number of arguments
@@ -114,6 +144,8 @@ typedef Value (*Native)(Value*, Location);
     X(exit_native,  exit,  1)\
     X(rand_native,  rand,  0)\
     X(pop,          pop,   1)\
+    X(ord,          ord,   1)\
+    X(chr,          chr,   1)\
 
 #define X(func, name, arity) func,
 Native natives[] = {
